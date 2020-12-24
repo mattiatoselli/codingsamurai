30f6edbff8146705ea0a110238bf7f682c5f6966
mattia <mattia@pop-os.localdomain>
2020-12-24 09:18:23 +0100

    ok

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,4 +1,6 @@
 var path = window.location.href
-if(path.includes("codingsamurai/codingsamurai")){
-    window.location.href = path.replace("codingsamurai/codingsamurai", "codingsamurai")
+var count = (temp.match(/codingsamurai/g) || []).length;
+if(count>1){
+    var param = path.split("/");
+    window.location.href = "https://mattiatoselli.github.io/codingsamurai/"+param[param.length-1];
 }
